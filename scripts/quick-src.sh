85243fe730cb8df9183774898d98f41fb54968b0
PieterGit <pieterg@gmail.com>
2017-04-15 20:32:34 +0200

    don't install nodejs-legacy and npm if they are already installed. add ntpdate to packages
    the nodejs-legacy package gives problems on systems that already have
    nodejs installed. So first test if nodejs and npm are available, update
    npm
    
    ntpdate can be used to sync with a timeserver (even offline), so is good
    to have available on a rig

--- scripts/quick-src.sh
+++ scripts/quick-src.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y git python python-dev python-software-properties python-numpy python-pip nodejs-legacy npm watchdog strace tcpdump screen acpid vim locate jq && \
+
+# Check the version of nodejs. If it's not installed install it
+NODEJS=`nodejs --version`
+if [ $? -ne 0 ]; then
+   # use 'apt-get install -y nodejs-legacy' for old version of node
+   curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+   sudo apt-get install -y nodejs
+else
+   echo "NodeJS version $NODEJS already installed"
+fi
+
+# Check the version of npm. If it's not installed install it
+NPM=`npm --version`
+if [ $? -ne 0 ]; then
+   sudo apt-get install -y npm
+else
+   echo "NPM version $NPM already installed"
+fi
+# Node comes with npm installed so you should have a version of npm. However, npm gets updated more frequently than Node does, so you'll want to make sure it's the latest version.
+npm install npm@latest -g
+
+
+
+sudo apt-get install -y git python python-dev python-software-properties python-numpy python-pip watchdog strace tcpdump screen acpid vim locate jq ntpdate && \
 ( curl -s https://bootstrap.pypa.io/ez_setup.py | sudo python ) && \
 sudo npm install -g json && \
 sudo easy_install -ZU setuptools && \
